7ac6157d4d76dad2fe0e0cb7575e590cc3177bcd
Your Name <you@example.com>
2022-11-10 11:36:00 +0300

    level 5

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
+[![Maintainability](https://api.codeclimate.com/v1/badges/6061463d69ef70318379/maintainability)](https://codeclimate.com/github/LAGZY/frontend-project-44/maintainability)
 [![Actions Status](https://github.com/LAGZY/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/LAGZY/frontend-project-44/actions)
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
 import readlineSync from 'readline-sync';
-export default function Welcomer(){
-console.log('Welcome to the Brain Games!')
-const name = readlineSync.question("May I have your name? ");
-console.log(name);
+
+function Welcomer() {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(name);
 }
+
+export default Welcomer;
