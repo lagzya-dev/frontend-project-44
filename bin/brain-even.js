2f4a2183fce8d6683d0d527f7b2cec88fbc35638
Your Name <you@example.com>
2022-11-10 11:34:36 +0300

    level 4

---
+++ bin/brain-even.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+console.log('Answer "yes" if the number is even, otherwise answer "no"Answer "yes" if the number is even, otherwise answer "no"');
+var num = Math.random * 10;
+console.log('Question: ' + num);
+const answer = readlineSync.question('You answer ');
+
+if(6 % 2 == 0 && answer == "yes"){
+
+}
