c945e32aeeba31c9226b4b4372ebf9473e8f49fa
лагзя <lagzya@gmail.com>
2023-03-20 18:27:08 +0300

    fixed eslint

--- src/primecheck.js
+++ src/primecheck.js
@@ -1,6 +1,6 @@
 export default function isPrime(num) {
   for (let i = 2; i <= num / 2; i += 1) {
-    if (num % i === 0) { return false; }
+    if (num % i === 0) { return false; };
   }
   return num > 1;
 }
