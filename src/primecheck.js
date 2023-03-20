9f5295a8270599ebe1cc004297f52f9cae9f25f6
лагзя <lagzya@gmail.com>
2023-03-20 18:35:17 +0300

    Spaces..

--- src/primecheck.js
+++ src/primecheck.js
@@ -1,7 +1,7 @@
 export default function isPrime(num) {
   for (let i = 2; i <= num / 2; i += 1) {
-    if (num % i === 0) { 
-      return false; 
+    if (num % i === 0) {
+      return false;
     }
   }
   return num > 1;
